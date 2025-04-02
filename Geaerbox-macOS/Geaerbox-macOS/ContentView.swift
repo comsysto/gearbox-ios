b2240a794572be9882a0f157183a6c944504ed10
FilipKisic <filip.kisic@racunarstvo.hr>
2025-04-02 10:52:30 +0200

    Added macOS app

---
+++ Geaerbox-macOS/Geaerbox-macOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Geaerbox-macOS
+//
+//  Created by Filip Kisić on 01.04.2025..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ Geaerbox-macOS/Geaerbox-macOS/Geaerbox_macOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Geaerbox_macOSApp.swift
+//  Geaerbox-macOS
+//
+//  Created by Filip Kisić on 01.04.2025..
+//
+
+import SwiftUI
+
+@main
+struct Geaerbox_macOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
